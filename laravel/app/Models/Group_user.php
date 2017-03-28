24ebe723b7307645befa4cc598aa715524db7986
Aurelio Decock <auredeco@student.arteveldehs.be>
2017-03-28 20:43:41 +0200

    finish migrations

--- laravel/app/Models/Group_user.php
+++ laravel/app/Models/Group_user.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Group extends Model
+class Group_user extends Model
 {
     //
 }
--- laravel/app/Models/Voter.php
+++ laravel/app/Models/Voter.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Referendum extends Model
+class Voter extends Model
 {
     //
 }
