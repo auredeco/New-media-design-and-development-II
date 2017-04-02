fb8d6c3f9e80912f56ecb3c475f3be246de0ebd1
basismit <basiel.smitz@student.arteveldehs.be>
2017-04-02 17:20:08 +0200

    toepassen admin theme bootstrap + herwerken dashboard

---
+++ laravel/resources/views/partials/navRight.blade.php
@@ -0,0 +1,4 @@
+<ul class="nav navbar-right top-nav">
+    <li ><a href="/settings">  <i class="fa fa-fw fa-gear"></i>  Settings</a></li>
+    <li ><a href="/login">  <i class="fa fa-fw fa-sign-in"></i> Login</a></li>
+</ul>
